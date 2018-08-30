0c52ac1aa25dde354459ecf2e9d26d5bb56a8a7a
fattazzo <fattazzo82@gmail.com>
2018-08-30 18:47:41 +0200

    .

--- src/app/components/app-controls/app-controls.component.ts
+++ src/app/components/app-controls/app-controls.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Season } from '../../domain/season';
-import { SeasonsService } from '../../services/seasons.service';
+import { Component, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { NbThemeService } from '@nebular/theme';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-controls',
   templateUrl: './app-controls.component.html',
   styleUrls: ['./app-controls.component.scss']
 })
-export class AppControlsComponent implements OnInit {
+export class AppControlsComponent implements OnDestroy {
 
-  constructor(private translate: TranslateService) { }
+  private alive = true;
 
-  ngOnInit() {
-    //this.translate.currentLang
+  currentTheme: string;
+
+  constructor(public translate: TranslateService, private themeService: NbThemeService) {
+    this.themeService.getJsTheme()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(theme => {
+        this.currentTheme = theme.name;
+      });
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
   }
 
 }
