44fcfbc50cdc8d03b2a33be538aac92b93d9c19d
fattazzo <fattazzo82@gmail.com>
2018-11-10 16:19:12 +0100

    .

--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -3,13 +3,89 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.scss']
+  styleUrls: ['./info.component.scss'],
 })
 export class InfoComponent implements OnInit {
+  androidAppImages: any[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const baseUrl =
+      'https://raw.githubusercontent.com/wiki/fattazzo/total-gp-world/images/';
 
+    this.androidAppImages = [];
+    this.androidAppImages.push({
+      source: `${baseUrl}sez_princ2.png`,
+      title: 'Main window',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}drivers.png`,
+      title: 'Drivers',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}driver_progress.png`,
+      title: 'Driver progress',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}driver_ranking.png`,
+      title: 'Driver ranking charts',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}driver_info.png`,
+      title: 'Driver info',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}constructors.png`,
+      title: 'Constructors',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}constructor_progress.png`,
+      title: 'Constructor progress',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}constructor_info.png`,
+      title: 'Constructor info',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}races.png`,
+      title: 'Races',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}race_results.png`,
+      title: 'Race results',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}race_stat_pittops.png`,
+      title: 'Race pit stat',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}race_stat_positions.png`,
+      title: 'Race positions stat',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}race_qualifications.png`,
+      title: 'Race qualifications',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}race_info.png`,
+      title: 'Race info',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}news.png`,
+      title: 'News',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}stats_drivers_wins.png`,
+      title: 'Drivers stats',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}stats_constructors_wins.png`,
+      title: 'Constructors stats',
+    });
+    this.androidAppImages.push({
+      source: `${baseUrl}stats_season_comparison.png`,
+      title: 'Season stats',
+    });
+  }
 }
--- src/app/pages/info/info.module.ts
+++ src/app/pages/info/info.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InfoRoutingModule } from './info-routing.module';
 import { InfoComponent } from './info.component';
+import { ThemeModule } from '../../@theme/theme.module';
+import { GalleriaModule } from 'primeng/galleria';
+import { GithubReleasesModule } from '../components/github/github-releases/github-releases.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  imports: [CommonModule, InfoRoutingModule],
+  imports: [
+    ThemeModule,
+    CommonModule,
+    InfoRoutingModule,
+    GalleriaModule,
+    GithubReleasesModule,
+    TranslateModule,
+  ],
   declarations: [InfoComponent],
 })
 export class InfoModule {}
