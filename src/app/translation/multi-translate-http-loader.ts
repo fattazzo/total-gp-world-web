2112287492677402a2ac4af13ced6ce088c5e12a
fattazzo <fattazzo82@gmail.com>
2019-01-04 14:47:32 +0100

    Add I18N settings files

--- src/app/translation/multi-translate-http-loader.ts
+++ src/app/translation/multi-translate-http-loader.ts
@@ -8,6 +8,7 @@ import { forkJoin } from 'rxjs';
 export function createMultiTranslateLoader(http: HttpClient) {
   return new MultiTranslateHttpLoader(http, [
     { prefix: './assets/i18n/base/', suffix: '.json' },
+    { prefix: './assets/i18n/settings/', suffix: '.json' },
     { prefix: './assets/i18n/ergast/finishing-status/', suffix: '.json' },
   ]);
 }
