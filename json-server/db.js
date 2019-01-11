9351f817a07875a5cbb1af35434730ecb13c6909
fattazzo <fattazzo82@gmail.com>
2019-01-11 13:52:52 +0100

    json server update

--- json-server/db.js
+++ json-server/db.js
@@ -10,6 +10,7 @@ module.exports = function() {
     qualifying: requireMany('./seasons/rounds/qualifying'),
     drivers: requireMany('./seasons/drivers'),
     constructors: requireMany('./seasons/constructors'),
+    circuits: requireMany('./seasons/circuits'),
     constructorsResult: requireMany('./seasons/constructors/results'),
   };
 };
